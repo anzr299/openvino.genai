725620f075cf544f41f120ecd1fa9c65103596f0
AamirNazir <36276944+anzr299@users.noreply.github.com>
2024-04-06 21:41:51 +0400

    Update greedy_causal_lm.cpp
    documented the changes in the code

--- text_generation/causal_lm/cpp/greedy_causal_lm.cpp
+++ text_generation/causal_lm/cpp/greedy_causal_lm.cpp
@@ -62,11 +62,9 @@ int main(int argc, char* argv[]) try {
     ov::Core core;
 
     auto tokenizer_model = core.read_model(std::string{argv[1]} + "/openvino_tokenizer.xml");
-
     core.add_extension(OPENVINO_TOKENIZERS_PATH);  // OPENVINO_TOKENIZERS_PATH is defined in CMakeLists.txt
     // tokenizer and detokenizer work on CPU only
-    ov::InferRequest tokenizer = core.compile_model(
-        std::string{argv[1]} + "/openvino_tokenizer.xml", "CPU").create_infer_request();
+    ov::InferRequest tokenizer = core.compile_model(tokenizer_model, "CPU").create_infer_request();
     auto [input_ids, attention_mask] = tokenize(tokenizer, argv[2]);
     ov::InferRequest detokenizer = core.compile_model(
         std::string{argv[1]} + "/openvino_detokenizer.xml", "CPU").create_infer_request();
@@ -94,10 +92,10 @@ int main(int argc, char* argv[]) try {
     position_ids.set_shape({BATCH_SIZE, 1});
     TextStreamer text_streamer{std::move(detokenizer)};
 
-    auto rt_info = tokenizer_model->get_rt_info();
+    auto rt_info = tokenizer_model->get_rt_info(); //Get the runtime info for the model
     int64_t SPECIAL_EOS_TOKEN;
 
-    if (rt_info.count("eos_token_id") > 0) {
+    if (rt_info.count("eos_token_id") > 0) { //check if the runtime information has a valid EOS token ID
         SPECIAL_EOS_TOKEN = rt_info["eos_token_id"].as<int64_t>();;
     } else {
         throw std::runtime_error("EOS token ID not found in model's runtime information.");
